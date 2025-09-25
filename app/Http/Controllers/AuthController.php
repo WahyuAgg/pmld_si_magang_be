eb643097b2570211451d7738dbde6b69fe6afd4a
WahyuAgg <wahyuagungbarokah@mail.ugm.ac.id>
2025-09-25 09:49:26 +0700

    feat: add middleware & authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        // Validasi input: gunakan username & password
+        $request->validate([
+            'username' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        // Coba login dengan username
+        if (!Auth::attempt($request->only('username', 'password'))) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $user = $request->user();
+
+        // Buat token
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        // Tentukan kapan access_token expired (misalnya 2 jam)
+        $token_expired = Carbon::now()->addHours(2);
+
+        // Simpan tanggal expired ke token terakhir
+        $user->tokens->last()->update([
+            'expires_at' => $token_expired,
+        ]);
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'expires_at' => $token_expired,
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        // Revoke semua token milik user
+        $request->user()->tokens->each(function ($token) {
+            $token->delete();
+        });
+
+        return response()->json(['message' => 'Logged out successfully.']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,25 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Auth Route
+|--------------------------------------------------------------------------
+*/
+
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+Route::get('/', function () {
+    return response()->json(['message' => 'Unauthenticated.'], 401);
+})->name('login');
