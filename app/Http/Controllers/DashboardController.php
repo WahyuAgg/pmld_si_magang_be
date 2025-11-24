b100f74d7027bc15ab0d7f4b6b57fb1d87f61e32
Naufal-Nafi <naufalabyu4869@gmail.com>
2025-11-24 16:41:30 +0700

    feat: dashboard data mitra

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\NilaiMitra;
 use Illuminate\Http\Request;
 use App\Models\Magang;
 use App\Models\Dosbing;
@@ -9,17 +10,17 @@ use App\Models\Mitra;
 
 class DashboardController extends Controller
 {
-    public function dashboardSummary()
+    public function fetchDashboardDataAdmin()
     {
         // 1. Hitung jumlah magang aktif berdasarkan tahun ajaran terbaru
         $tahunAjaranTerbaru = Magang::latest('tahun_ajaran')->value('tahun_ajaran');
-        $SemesterMagangTerbaru = Magang::latest('semester_magang')->value('semester_magang'); 
+        $SemesterMagangTerbaru = Magang::latest('semester_magang')->value('semester_magang');
 
 
         if ($tahunAjaranTerbaru) {
             $jumlahMagangAktif = Magang::where('tahun_ajaran', $tahunAjaranTerbaru)
-                                        // ->where('semester_magang', $SemesterMagangTerbaru)
-                                        ->count();
+                // ->where('semester_magang', $SemesterMagangTerbaru)
+                ->count();
         } else {
             $jumlahMagangAktif = 0;
         }
@@ -33,8 +34,27 @@ class DashboardController extends Controller
         // Return JSON gabungan
         return response()->json([
             'jumlah_mahasiswa_aktif_magang' => $jumlahMagangAktif,
-            'jumlah_mitra'        => $jumlahMitra,
-            'jumlah_dosbing'      => $jumlahDosbing,
+            'jumlah_mitra' => $jumlahMitra,
+            'jumlah_dosbing' => $jumlahDosbing,
+        ], 200);
+    }
+
+    public function fetchDashboardDataMitra()
+    {
+        // Hitung jumlah mahasiswa magang untuk mitra yang sedang login
+        $mitraId = auth()->user()->mitra->mitra_id;
+
+        $jumlahMahasiswaMagang = Magang::where('mitra_id', $mitraId)
+            ->where('status_magang', 'berlangsung')
+            ->count();
+
+        $jumlahPenilaian = NilaiMitra::whereHas('magang', function ($query) use ($mitraId) {
+            $query->where('mitra_id', $mitraId);
+        })->count();
+
+        return response()->json([
+            'jumlah_mahasiswa_magang' => $jumlahMahasiswaMagang,
+            'jumlah_penilaian' => $jumlahPenilaian,
         ], 200);
     }
 }
